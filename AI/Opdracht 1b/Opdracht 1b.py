165e98e786289e8d47bae38fbbadaf9bb0980c46
khaitam2001 <khaitam2001@gmail.com>
2021-02-03 20:15:26 +0100

    Functies en pseudocode toegevoegd

---
+++ AI/Opdracht 1b/Opdracht 1b.py
@@ -0,0 +1,30 @@
+""" Opdracht 1b """
+
+
+def textGUI():
+    """ Deze functie zorgt ervoor dat de gebruiker text kan zien dat een spelbord moet voorstellen. Verder worden hier
+    ook inputs geplaatst om de gok van de speler op te slaan"""
+    pass
+
+
+def checkAnswer(guessedanswer, correctanswer):
+    """ Deze functie returnt het aantal correct gegokte antwoorden. """
+    pass
+
+
+def shapiroAI():
+    """ Deze functie zorgt ervoor dat er een tegenstander is waar de gebruiker tegen kan spelen. De manier waarop
+    de AI keuzes maakt, is gebaseerd op de strategie van Shapiro"""
+    pass
+
+
+def onestepaheadAI():
+    """ Deze functie zorgt ervoor dat er een tegenstander is waar de gebruiker tegen kan spelen. De manier waarop
+    de AI keuzes maakt, is gebaseerd op een stap vooruit kijken. """
+    pass
+
+
+def compleetWillekeurigAI():
+    """ Deze functie zorgt ervoor dat er een tegenstander is waar de gebruiker tegen kan spelen. De manier waarop
+    de AI keuzes maakt, is gebaseerd op compleet willekeurig kiezen. """
+    pass
