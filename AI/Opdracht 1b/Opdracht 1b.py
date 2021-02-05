b07b88760344bc00ae7af3619c5391254a60ab00
khaitam2001 <khaitam2001@gmail.com>
2021-02-05 10:31:00 +0100

    generaterandomcode en generatecombinationslist functies toegevoegd

--- AI/Opdracht 1b/Opdracht 1b.py
+++ AI/Opdracht 1b/Opdracht 1b.py
@@ -1,10 +1,38 @@
 """ Opdracht 1b """
 
+from random import randrange
+import itertools
+import string
 
-def textGUI():
+
+def textterminal():
     """ Deze functie zorgt ervoor dat de gebruiker text kan zien dat een spelbord moet voorstellen. Verder worden hier
     ook inputs geplaatst om de gok van de speler op te slaan"""
     pass
+
+
+def generaterandomcode(number_of_colors=None):
+    if number_of_colors == None:
+        number_of_colors = generatecombinationslist(6)
+    else:
+        number_of_colors = generatecombinationslist(number_of_colors)
+    return number_of_colors[randrange(0, len(number_of_colors))]
+
+
+def generatecombinationslist(number_of_colors):
+    """ Genereer alle mogelijke combinaties. Returnt een lijst met alle combinaties"""
+    # SOURCE = https://stackoverflow.com/questions/464864/how-to-get-all-possible-combinations-of-a-list-s-elements
+    list = []
+    for i in range(number_of_colors):
+        list.append(string.ascii_lowercase[i])
+    all_combinations = []
+    list_permutations = itertools.permutations(list, len(list))
+    for each_permutation in list_permutations:
+        all_combinations.append(each_permutation)
+    return all_combinations
+
+# print(generatecombinationslist(5))
+# print(generaterandomcode(5))
 
 
 def checkAnswer(guessedanswer, correctanswer):
@@ -24,7 +52,8 @@ def onestepaheadAI():
     pass
 
 
-def compleetWillekeurigAI():
+def eenkleurAI():
     """ Deze functie zorgt ervoor dat er een tegenstander is waar de gebruiker tegen kan spelen. De manier waarop
-    de AI keuzes maakt, is gebaseerd op compleet willekeurig kiezen. """
+    de AI keuzes maakt, is gebaseerd op een kleur kiezen. Daarna gebaseerd op de feedback worden andere keuzes gemaakt
+    """
     pass
