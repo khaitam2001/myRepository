e9075147d675d7ed8bf05d8abc61471b7f06dff1
khaitam2001 <khaitam2001@gmail.com>
2021-06-01 23:23:02 +0200

    Schaakboord GUI toegevoegd en klasse Rook en BlackPawn en WhitePawn toegevoegd

--- AI/Opdracht 1a/Opdracht 10.py
+++
@@ -1,9 +0,0 @@
-""" Opdracht 10 - Fibonaci """
-
-
-def fibonaci(n, previousnumber=0, currentnumber=1):
-    if n == 1:
-        return currentnumber
-    return fibonaci(n - 1, currentnumber, previousnumber + currentnumber)
-
-print(fibonaci(9))
